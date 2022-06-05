182a0a5963cf0d0ebc999eba227c65ebc0ffccfe
srijaniiserprinceton <sbdas@princeton.edu>
2022-06-05 06:24:38 -0400

    Almost done with setting up the flow kernels.

--- src/compute_kernels.py
+++ src/compute_kernels.py
@@ -2,7 +2,14 @@ import numpy as np
 import scipy
 
 class CartKerns:
-    def __init__(self, z, k_grid, kmin, kmax, absq_range):
+    def __init__(self, z, n, n_, k_grid, kmin, kmax,
+                 absq_range, bsp_basis, rho):
+        self.z = z 
+        self.rho = rho
+        self.fj = bsp_basis
+        
+        self.n = n
+        self.n_ = n_
         self.kx = k_grid
         self.ky = k_grid
         
@@ -20,128 +27,162 @@ class CartKerns:
                                (self.abskg > self.kmin), 1, 0)
         self.nmodes = np.where(self.k_mask==True)[0].size
         
-    def grad_z(self):
-        # get the derivatives                                                                
-        dz_H = np.gradient(Xi_h, z, axis=1)
-        dz_V = np.gradient(Xi_z,z,axis=1)
-        dz_H_ = np.gradient(Xi_hp,z,axis=1)
-        dz_V_ = np.gradient(Xi_zp,z,axis=1)
-        dz_rho = np.gradient(rho,z)
-        dz_fj = np.gradient(fj,z,axis=1)
-        
-        return dz_H, dz_V, dz_H_, dz_V_, dz_rho, dz_fj
-
-    def compute_kernels(self, q_vec_ind, n, n_):
-        # kernels will be computed for this qx and qy
-        qx_ind = q_vec_ind[0]
-        qy_ind = q_vec_ind[1] 
-        
         # indices of kx and ky for which 600 < |k|R < 1500 
         kx_ind, ky_ind = np.where(self.k_mask==1)
         
         # load eigenfunctions of k
         eig = np.load('gyreEigenFunctions/eigs{:02d}.npz'.format(n))
-        H = eigt['Xi_h'] ; V = eig['Xi_z'] ; k = eig['eig_k']
+        self.raw_H = eig['Xi_h']
+        self.raw_V = eig['Xi_z']
+        self.raw_k = eig['eig_k']
         
         # load eigenfunctions of k'
-        eig_ = np.load('gyreEigenFunctions/eigs{:02d}.npz'.format(npr))
-        H_ = eig_['Xi_h'] ; V_ = eig_['Xi_z'] ; k_ = eig_['eig_k']
+        eig_ = np.load('gyreEigenFunctions/eigs{:02d}.npz'.format(n_))
+        self.raw_H_ = eig_['Xi_h']
+        self.raw_V_ = eig_['Xi_z']
+        self.raw_k_ = eig_['eig_k']
+
+        # gradients needed
+        self.raw_dzH, self.raw_dzV, self.raw_dzH_, self.raw_dzV_,\
+        dz_rho, dz_fj = self.grad()
+
+        # the miscellaneous interpolation functions needed
+        self.ITP_H, self.ITP_V, self.ITP_H_,\
+        self.ITP_V_, self.ITP_dzH, self.ITP_dzV,\
+        self.ITP_dzH_, self.ITP_dzV_ = interpolar_eigs()        
         
-        # get the necessary derivatives
-        dz_H, dz_V, dz_H_, dz_V_, dz_rho, dz_fj = self.grad_z()
+    def grad_z(self):
+        # get the derivatives                                                                
+        dzH = np.gradient(self.raw_H, self.z, axis=1)
+        dzV = np.gradient(self.raw_V, self.z, axis=1)
+        dzH_ = np.gradient(self.raw_H_, self.z, axis=1)
+        dzV_ = np.gradient(self.raw_V_, self.z, axis=1)
+        dzrho = np.gradient(self.rho, self.z)
+        dzfj = np.gradient(self.fj, self.z, axis=1)
         
-        #.....interpolate because the quantities in the eigenfunction
+        return dzH, dzV, dzH_, dzV_, dzrho, dzfj
+
+    def interpolate_eigs(self):
+        # interpolate because the quantities in the eigenfunction
         # files are not always defined for the same values of kx,ky as we need
-        ITP_Xi_h   = scipy.interpolate.interp2d(eig_k, z, Xi_h.T, 
-                                                kind='linear', 
-                                                bounds_error=False,fill_value=np.nan)
-        ITP_Xi_z   = scipy.interpolate.interp2d(eig_k, z, Xi_z.T, 
-                                                kind='linear',
-                                                bounds_error=False,fill_value=np.nan)
-        ITP_dzXi_h = scipy.interpolate.interp2d(eig_k, z, dzXi_h.T,
-                                                kind='linear',
-                                                bounds_error=False,fill_value=np.nan)
-        ITP_dzXi_z = scipy.interpolate.interp2d(eig_k, z, dzXi_z.T,
-                                                kind='linear',
-                                                bounds_error=False,fill_value=np.nan)
+        ITP_H   = scipy.interpolate.interp2d(self.raw_k, z, self.raw_H.T, 
+                                             kind='linear', 
+                                             bounds_error=False,fill_value=np.nan)
+        ITP_V   = scipy.interpolate.interp2d(self.raw_k, z, self.raw_V.T, 
+                                             kind='linear',
+                                             bounds_error=False,fill_value=np.nan)
         
-        ITP_Xi_hp   = scipy.interpolate.interp2d(eig_kp, z, Xi_hp.T,
-                                                 kind='linear',
-                                                 bounds_error=False,fill_value=np.nan)
-        ITP_Xi_zp   = scipy.interpolate.interp2d(eig_kp, z, Xi_zp.T,
-                                                 kind='linear',
-                                                 bounds_error=False,fill_value=np.nan)
-        ITP_dzXi_hp = scipy.interpolate.interp2d(eig_kp, z, dzXi_hp.T,
-                                                 kind='linear',
-                                                 bounds_error=False,fill_value=np.nan)
-        ITP_dzXi_zp = scipy.interpolate.interp2d(eig_kp, z, dzXi_zp.T,
-                                                 kind='linear',
-                                                 bounds_error=False,fill_value=np.nan)
+        ITP_H_   = scipy.interpolate.interp2d(self.raw_k_, z, self.raw_H_.T,
+                                              kind='linear',
+                                              bounds_error=False,fill_value=np.nan)
+        ITP_V_   = scipy.interpolate.interp2d(self.raw_k_, z, self.raw_V_.T,
+                                              kind='linear',
+                                              bounds_error=False,fill_value=np.nan)
         
+        ITP_dzH   = scipy.interpolate.interp2d(self.raw_k, z, self.raw_dzH.T,
+                                               kind='linear',
+                                               bounds_error=False,fill_value=np.nan)
+        ITP_dzV   = scipy.interpolate.interp2d(self.raw_k, z, self.raw_dzV.T,
+                                               kind='linear',
+                                               bounds_error=False,fill_value=np.nan)
+
+        ITP_dzH_   = scipy.interpolate.interp2d(self.raw_k_, z, self.raw_dzH_.T,
+                                                kind='linear',
+                                                bounds_error=False,fill_value=np.nan)
+        ITP_dzV_   = scipy.interpolate.interp2d(self.raw_k_, z, self.raw_dzV_.T,
+                                                kind='linear',
+                                                bounds_error=False,fill_value=np.nan)
+
+        return ITP_H, ITP_V, ITP_H_, ITP_V_, ITP_dzH, ITP_dzV, ITP_dzH_, ITP_dzV_
+        
+    
+    # getting all the kernels for a certain q
+    def compute_kernels(self, q_vec_ind):
+        # kernels will be computed for this qx and qy
+        qx_ind = q_vec_ind[0]
+        qy_ind = q_vec_ind[1] 
+                
         Poloidal_kernel = np.zeros((nmodes,fj.shape[0]))
         
-        for idx,(kxi,kyi) in enumerate(zip(kx_ind,ky_ind)):
-            k_vec = np.array([kx[kxi],ky[kyi]])
+        for idx, (kxi, kyi) in enumerate(zip(kx_ind, ky_ind)):
+            # k vector, k unit vector, k norm
+            k_vec = np.array([self.kx[kxi], self.ky[kyi]])
             k_hat = k_vec/np.linalg.norm(k_vec)
             abs_k = np.linalg.norm(k_vec)
-            
-            Xi_h_k  = ITP_Xi_h (abs_k,z).squeeze()
-            Xi_z_k  = ITP_Xi_z (abs_k,z).squeeze()
-            dzXi_h_k = ITP_dzXi_h(abs_k,z).squeeze()
-            dzXi_z_k = ITP_dzXi_z(abs_k,z).squeeze()
-            
-            if (kxi+qx_ind < 0) or (kxi+qx_ind > len(kx)) or (kyi+qy_ind < 0) or (kyi+qy_ind > len(ky)):
-                continue;
-                
+
             # compute |q|,q,k',|k'| and k'_hat
             # q = k' - k
             kxp_ind = kxi + qx_ind
             kyp_ind = kyi + qy_ind
             
-            if kxp_ind > (len(kx)-1) or kyp_ind > (len(ky)-1):
-                # if qx is outside of the grid, continue
-                continue;
-                
-            qx = kx[kxp_ind] - kx[kxi]
-            qy = ky[kyp_ind] - ky[kyi]
-            abs_q = np.sqrt(qx**2 + qy**2)
-            q_vec = np.array([qx,qy])
-            
+            # if k' vector is outside of the grid
+            if((kxp_ind > (len(kx)-1)) or (kyp_ind > (len(ky)-1))):
+                continue
+
+            # k' vector, k' unit vector, k' norm                                            
             kxp = kx[kxp_ind]
             kyp = ky[kyp_ind]
+            kp_vec = np.array([kxp, kyp])
             abs_kp = np.sqrt(kxp**2 + kyp**2)
-            kp_vec = np.array([kxp,kyp])
-            kp_hat = kp_vec/np.linalg.norm(kp_vec)
-            
-            if np.sum(np.isnan(kp_hat)):
-                continue;
-                
+            kp_hat = kp_vec/abs_kp
+        
+            # q vector, q unit vector, q norm                                             
+            qx = kx[kxp_ind] - kx[kxi]
+            qy = ky[kyp_ind] - ky[kyi]
+            q_vec = np.array([qx, qy])
+            abs_q = np.sqrt(qx**2 + qy**2)
+            q_hat = q_vec/abs_q
+
             # if abs_q is outside a desired range continue
-            if abs_q > absq_range[1] or abs_q < absq_range[0] :
-                continue;
+            if((abs_q > self.absq_range[1]) or (abs_q < self.absq_range[0])):
+                continue
                 
-            Xi_h_kp   = ITP_Xi_hp (abs_kp,z).squeeze()
-            Xi_z_kp   = ITP_Xi_zp (abs_kp,z).squeeze()
-            dzXi_h_kp = ITP_dzXi_hp(abs_kp,z).squeeze()
-            dzXi_z_kp = ITP_dzXi_zp(abs_kp,z).squeeze()
             
+            # constructing the interpolated eigenfunctions for k
+            H_k  = self.ITP_H(abs_k, self.z).squeeze()
+            V_k  = self.ITP_V(abs_k, self.z).squeeze()
+            dzH_k = self.ITP_dzH(abs_k, self.z).squeeze()
+            dzV_k = self.ITP_dzV(abs_k, self.z).squeeze()
+                                    
+            # constructing the interpolated eigenfunctions for k'
+            H__k  = self.ITP_H_(abs_kp, self.z).squeeze()
+            V__k  = self.ITP_V_(abs_kp, self.z).squeeze()
+            dzH__k = self.ITP_dzH_(abs_kp, self.z).squeeze()
+            dzV__k = self.ITP_dzV_(abs_kp, self.z).squeeze()
+
+
+            # THIS CONDITION IS WRONG I THINK
+            if np.sum(np.isnan(kp_hat)):
+                continue
+                            
+            # WHY SHOULD THIS BE NEEDED IF THE INTERPOLATION WORKS FINE
             # If there is an interpolation error in the eigenfunctions, continue
             if np.sum(np.isnan([Xi_h_kp,Xi_z_kp,dzXi_h_kp,dzXi_z_kp])) != 0:
-                continue;
+                continue
                 
-            kdotkp_hat = np.dot(k_hat,kp_hat)
-            kdotq      = np.dot(k_vec,q_vec)
+
+            # getting the poloidal flow kernel
+            cos_khk_h = np.dot(k_hat,kp_hat)
+            cos_kq = np.dot(k_vec,q_vec)
             
-            scale_height = -rho/drho
-            term1 = (abs_q**2*fj)*(kdotkp_hat*dzXi_h_k*np.conj(Xi_h_kp) +\
-                                   dzXi_z_k*np.conj(Xi_z_kp))
-            term2 = -np.dot(k_vec,q_vec)*(dfj-fj/scale_height) *\
-                    (kdotkp_hat*Xi_h_k*np.conj(Xi_h_kp) + Xi_z_k*np.conj(Xi_z_kp))
+            Poloidal_kernel[idx] = self.flow_kern(cos_khk_h, cos_kq, )        
             
-            # compute integrand for P matrix
-            integrand_P = -(term1 + term2)
-            LZ = 1/abs_q if abs_q != 0 else 1
-        
-            Poloidal_kernel[idx] = (np.trapz(integrand_P*rho[None,:],z,axis=1))*LZ**2
             
         return Poloidal_kernel.astype(np.float32)
+
+    def flow_kern(self):
+        # getting the poloidal flow kernel                                                 
+        kdotkp_hat = np.dot(k_hat,kp_hat)
+        kdotq      = np.dot(k_vec,q_vec)
+        
+        scale_height = -rho/drho
+        term1 = (abs_q**2*fj)*(kdotkp_hat*dzXi_h_k*np.conj(Xi_h_kp) +\
+                               dzXi_z_k*np.conj(Xi_z_kp))
+        term2 = -np.dot(k_vec,q_vec)*(dfj-fj/scale_height) *\
+                (kdotkp_hat*Xi_h_k*np.conj(Xi_h_kp) + Xi_z_k*np.conj(Xi_z_kp))
+        
+        # compute integrand for P matrix                                                   
+        integrand_P = -(term1 + term2)
+        LZ = 1/abs_q if abs_q != 0 else 1
+        
+        return (np.trapz(integrand_P*rho[None,:],z,axis=1))*LZ**2
